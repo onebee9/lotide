b328cf8ed2c0a2840b56fd3b71cd89b4a2294eb5
ibifuro odu <oduibifuro@gmail.com>
2021-11-24 14:23:35 -0500

    output a message after comparing arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(arr1,arr2) {
+    // if length is not equal
+      if (arr1.length !== arr2.length) {
+        return false;
+      } else {
+        // comparing each element of array
+        for (let i = 0; i < arr1.length; i++)
+          if (arr1[i] !== arr2[i])
+            return false;
+      }
+      return true;
+    };
+
+const assertArraysEqual = function(arr1,arr2) {
+if (eqArrays(arr1,arr2)){
+    console.log(`These arrays are equal`);
+}
+else {
+    console.log(`These arrays are not equal`);
+}
+};
+
+console.log(assertArraysEqual([234,2,3,4],[234,2,3,4]));
+console.log(assertArraysEqual([23,2,3,4],[234,2,3,4]));
+console.log(assertArraysEqual([23,2,3,4],[234,2,3,5]));
