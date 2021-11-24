03e2384d514db4c23a34c610a446e1a0b5ac73d0
ibifuro odu <oduibifuro@gmail.com>
2021-11-24 00:02:41 -0500

    cleanup

--- head.js
+++ head.js
@@ -1,18 +1,15 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`:) Assertion Passed : ${actual} === ${expected}`);
-    } else {
-      console.log(`:( Assertion Failed : ${actual} !== ${expected}`);
-    }
-    return;
-  };
-const head = function(arr) {  
-
-return arr[0];
-
+  if (actual === expected) {
+    console.log(`:) Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed : ${actual} !== ${expected}`);
   }
+  return;
+};
+const head = function(arr) {
+  return arr[0];
+};
   
-  assertEqual(head([5,6,7]), 5);
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-  assertEqual(head([]), 5);
-  
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
