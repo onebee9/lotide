386b65f0ec8aaa19536d8e9d4fb2b9f125c653d0
ibifuro odu <oduibifuro@gmail.com>
2021-11-23 21:37:45 -0500

    checks if variables match and outputs a message

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`:) Assertion Passed : ` + [actual] + ` === ` + [expected]);
+  } else {
+    console.log(`:( Assertion Failed : ` + [actual] + ` !== ` + [expected]);
+  }
+  return;
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
