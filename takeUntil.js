2e02a051dd8370d63050b74b6b799f830bacf6e6
ibifuro odu <oduibifuro@gmail.com>
2021-12-02 19:53:54 -0500

    check for and return items before a point

---
+++ takeUntil.js
@@ -0,0 +1,24 @@
+function takeUntil(data, callback){
+    var response = [];
+    data.every(element => {
+        if(!callback(element)){
+            response.push(element);
+            return true;
+        }else{
+            return false;
+        }
+    });
+
+    return response;
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
