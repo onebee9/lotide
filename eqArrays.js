e0cd356a2d82b348c1d625a5f81409c500273c3b
ibifuro odu <oduibifuro@gmail.com>
2021-12-04 10:21:49 -0500

    refactored to haave seperate test files

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`:) Assertion Passed : ${actual} === ${expected}`);
-  } else {
-    console.log(`:( Assertion Failed : ${actual} !== ${expected}`);
-  }
-  return;
-};
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(1, 2);
-  
 const eqArrays = function(arr1,arr2) {
 // if length is not equal
   if (arr1.length !== arr2.length) {
     return false;
   } else {
-    // comparing each element of array
+// comparing each element of array
     for (let i = 0; i < arr1.length; i++)
       if (arr1[i] !== arr2[i])
         return false;
   }
   return true;
 };
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
+
+//export function
+module.exports = eqArrays;
