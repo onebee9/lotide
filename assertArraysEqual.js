9568d372ca3a32a7ace8f9a694122469fae77004
ibifuro odu <oduibifuro@gmail.com>
2021-12-04 10:27:32 -0500

    refactored to move test cases to a seperate file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,3 @@
-const eqArrays = function(arr1,arr2) {
-    // if length is not equal
-      if (arr1.length !== arr2.length) {
-        return false;
-      } else {
-        // comparing each element of array
-        for (let i = 0; i < arr1.length; i++)
-          if (arr1[i] !== arr2[i])
-            return false;
-      }
-      return true;
-    };
-
 const assertArraysEqual = function(arr1,arr2) {
 if (eqArrays(arr1,arr2)){
     console.log(`These arrays are equal`);
@@ -19,3 +6,5 @@ else {
     console.log(`These arrays are not equal`);
 }
 };
+
+module.exports = assertArraysEqual;
